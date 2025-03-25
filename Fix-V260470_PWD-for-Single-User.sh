afd7b94f68f65cee1aeb40d3a27007a575f24251
chiefbobkat <chiefbobkat@gmail.com>
2025-03-25 08:41:06 -0600

    Add password confirmation to the script

--- Fix-V260470_PWD-for-Single-User.sh
+++ Fix-V260470_PWD-for-Single-User.sh
@@ -5,6 +5,17 @@ echo "Enter password for GRUB superuser (will not be displayed):"
 read -s password
 echo ""
 
+# Prompt user to confirm the password
+echo "Confirm password for GRUB superuser (will not be displayed):"
+read -s password_confirm
+echo ""
+
+# Verify that the passwords match
+if [ "$password" != "$password_confirm" ]; then
+    echo "Error: Passwords do not match. Exiting."
+    exit 1
+fi
+
 # Generate hashed password using grub-mkpasswd-pbkdf2
 # Since grub-mkpasswd-pbkdf2 doesn't accept stdin directly, we'll use a workaround
 # by echoing the password twice with newlines and piping it to the command.
